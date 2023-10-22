64dc0b44d1f442090eb65fce437637bb45cf2e07
Marlon <143544599+Marllonlp@users.noreply.github.com>
2023-10-22 09:51:51 -0300

    Add files via upload

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,36 +1,39 @@
 var menu = `
 <nav class="navbar navbar-expand-lg">
-<div class="container">
-    <a class="navbar-brand ms-md-5"><img src="imagens/logo.png"></a>
-    <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="border-dark navbar-toggler-icon"></span>
-    </button>
+    <div class="container">
+        <a class="navbar-brand ms-md-5"><img src="imagens/logo.png"></a>
+        <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="border-dark navbar-toggler-icon"></span>
+        </button>
 
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-           <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
+            </div>
+
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-5">
+                        <li class="nav-item">
+                            <a class="nav-link ms-md-4" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ms-md-4" href="destinos.html">Destino</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ms-md-4" href="promocao.html">Promoções</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ms-md-4" href="contato.html">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <ul class="navbar-nav justify-content-center flex-grow-1 pe-5">
-                    <li class="nav-item">
-                        <a class="nav-link ms-md-4" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link ms-md-4" href="destinos.html">Destino</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link ms-md-4" href="promocao.html">Promoções</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link ms-md-4" href="contato.html">Contato</a>
-                    </li>
-                </ul>
-        </nav>
-
+    </div>
+</nav>
 
         <style>
             .navbar-toggler-icon {
